140bbe43edfe9ed277bd6478b6ff5a999a05a4d9
Abhik <abhikghosh87@gmail.com>
2022-04-07 19:59:07 +0200

    added provider

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.10.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  # Configuration options
+}
+
+
+
+
